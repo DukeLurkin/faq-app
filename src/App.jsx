47f454e0389310957e962afc5214f6206ed6dcc9
DukeLurkin <durkin.lukas@gmail.com>
2025-08-25 17:09:28 -0400

    added functionality to the toggle dark more button, it now toggles between light and dark modes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from 'react'
 import FAQList from "./components/FAQList";
 
 const App = () => {
+
+  const [darkMode, setDarkMode] = useState(false)
+
+  useEffect (() => {
+    if (darkMode) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  },[darkMode])
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode)
+  }
+
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-50 via-gray-100 to-gray-50">
+    <div className="min-h-screen bg-gradient-to-br from-gray-50 via-gray-100 to-gray-50 dark:from-gray-950 dark:via-gray-900 dark:to-gray-950">
       <div className="container mx-auto py-12">
         <header className="text-center mb-12">
           <h1 className="text-4xl md:text-5xl font-extrabold mb-4 bg-gradient-to-r from-blue-600 via-indigo-600 to-purple-600 inline-block text-transparent bg-clip-text">
@@ -14,7 +29,7 @@ const App = () => {
           </p>
         </header>
       </div>
-      <FAQList />
+      <FAQList toggleDarkMode={toggleDarkMode}/>
     </div>
   );
 };
